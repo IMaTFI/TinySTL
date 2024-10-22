33a9dda38e1d0cd2b440d749517a1718b06ecc7b
IMaTFI <1183162088@qq.com>
2024-10-22 15:51:04 +0800

    modified test

--- test/main.cpp
+++ test/main.cpp
@@ -1,10 +1,17 @@
-#include"Queue.h"
-#include"Vector.h"
+#include<iostream>
+#include<chrono>
+#include<memory>
+#include<algorithm>
+#include<vector>
+
+#include"allocator.h"
 
 int main(){
-    Vector v;
-    Queue q;
-    v.doSomeThing();
-    q.doSomeThing();
+    tinystl::allocator<std::string> as;
+    auto p = as.allocate(10);
+    as.construct(p, 100, 's');
+    std::cout<<*p<<std::endl;
+    std::cout<<sizeof(std::string)<<std::endl;
+    as.deallocate(p);
     return 0;
 }
